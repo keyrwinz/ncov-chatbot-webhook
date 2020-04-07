843ee433c80a7a05f1eabb9c62d1ca3b2f58533d
keyrwinz <keyrwinfelisilda@gmail.com>
2020-04-07 17:00:37 +0800

    reply functionality to fb messenger

--- Guide/guide.js
+++ Guide/guide.js
@@ -1,27 +1,28 @@
-export const Help = `
-NAME:
-  NCOV Chatbot2020
+module.exports = {
+  Help: `
+NCOV Chatbot2020
 
 DESCRIPTION:
-  This is a chatbot that would report current information
-  regarding the COVID19 virus
+    This is a chatbot that would 
+    report current information
+    regarding the COVID19 virus
 
 EXAMPLES:
-  'ncov' => #shows COVID19 cases summary
-  'ncov philippines' => #shows COVID19 cases in the 'Philippines'
+    'ncov' => #shows COVID19 cases summary
+    'ncov philippines' => #shows COVID19 cases in the 'PH'
 
 AUTHOR:
-  keyrwinfelisilda@gmail.com
+    keyrwinfelisilda@gmail.com
 
 GITHUB:
-  https://github.com/keyrwinz/ncov-chatbot-webhook
+    https://github.com/keyrwinz/ncov-chatbot-webhook
 
 COMMANDS:
-   help	
-   ncov  
-   ncov <country>
-`
-
-export const Greetings = `Hi there! this is a chatbot for COVID19 virus. Try inputting 'help' for more info.`
-export const ErrorCountry = `Country not found. Try to check the spelling and try again`
-export const ErrorCountryWithoutDash = `Error country name. Try using '-' ex. south korea => 'south-korea'`
+    help
+    ncov
+    ncov <country>
+  `,
+  Greetings: `Hi there! this is a chatbot for COVID19 virus. Try inputting 'help' for more info.`,
+  ErrorCountry: `Country not found. Try to check the spelling and try again`,
+  ErrorCountryWithoutDash: `Error country name. Try using '-' ex. south korea => 'south-korea'`,
+}
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const bodyparser = require('body-parser')
 const dotenv = require('dotenv');
 dotenv.config();
 
+//predefined messages
+const { Help, Greetings, ErrorCountry, ErrorCountryWithoutDash } = require('./Guide/guide')
+
 const app = express()
 const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN
 const HUB_VERIFY_TOKEN = process.env.HUB_VERIFY_TOKEN
@@ -28,8 +31,45 @@ app.post("/webhook", (req,res) => {
   let message_events = req.body.entry[0].messaging
 	message_events.forEach( (entry) => {
     if (entry.message && entry.message.text) {
+      console.log(`message recieved with id: ${entry.sender.id} and text: ${entry.message.text}`)
       ProcessText(entry.sender.id, entry.message.text)
       res.sendStatus(200);
     }
   });
 });
+
+const ProcessText = (id, message) => {
+  let str = message.toLowerCase().trim().split(" ").filter(Boolean)
+  let text;
+
+  console.log('processing text...')
+  if (str[0] === 'ncov') {
+    text = 'NCOV command'
+  } else if (str[0] === 'help') {
+    text = Help
+  } else {
+    text = "Hi there! type 'help' to show documentation"
+  }
+
+  SendText(id, text)
+}
+
+const SendText = (id, text) => {
+  console.log('replying...')
+  request({
+    url: 'https://graph.facebook.com/v6.0/me/messages',
+    qs: { access_token: FB_PAGE_TOKEN },
+    method: 'POST',
+    json: {
+      recipient: { id: id },
+      message: { text: text }
+    }
+  })
+}
+
+const GetCovidInfo = (country = null) => {
+  
+}
+
+
+
