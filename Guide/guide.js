984dadf38578dc421b49e64670e96f1670b080ec
keyrwinz <keyrwinfelisilda@gmail.com>
2020-04-09 16:08:30 +0800

    small changes

--- Guide/guide.js
+++ Guide/guide.js
@@ -1,15 +1,16 @@
 module.exports = {
   Help: `
 EXAMPLES:
-    'ncov' => #shows COVID19 cases summary
-    'ncov philippines' => #shows COVID19 cases in the PH
-    'ncov yesterday ph' => #shows yesterday's COVID19
-          cases in the Philippines'
+    'ncov' => #shows world cases summary
+
+    'ncov philippines' => #shows cases in the PH
+
+    'ncov yesterday ph' => #shows yesterday's cases in the Philippines'
 
 COMMANDS:
-    help // show commands
-    info // show app info
-    ncov // show world covid cases
+    help
+    info
+    ncov
     ncov <country>
     ncov yesterday <country>
   `,
@@ -26,15 +27,9 @@ NAME:
     SCOVIDo bot
 
 DESCRIPTION:
-    This is a simple chatbot that 
-    would report updated information
-    regarding the COVID-19 virus cases.
-
-    This app relies on 'NovelCOVID API' to access
-    real-time critical data. We encourage you to
-    seek more informations on other sources. 
-    Type 'info' to show app information and other
-    available sources.
+    This is a simple chatbot that would report updated information regarding the COVID-19 virus cases.
+    This app relies on 'NovelCOVID API' to access real-time critical data. We encourage you to seek more informations on other sources. 
+    Type 'info' to show app information and other available sources.
 
 API SOURCES:
     https://corona.lmao.ninja/
@@ -43,9 +38,10 @@ API SOURCES:
 OTHER SOURCES:
     https://www.worldometers.info/coronavirus/
 
-REPO: Please go to 'about' section
+REPO: Please go to 'about' section on this page
   `,
   ErrorCountry: `Please check your spelling and avoid using dash`,
+  ErrorYesterdayQuery: `Please input country. Type 'help' for more info 🙂`,
   MessageForAttachments: `Hi there 👋.. just to let you know you don't need to send attachments to use this app 😊`,
   DefaultMessage: `Hi there 👋! try typing 'help' to show documentation 😊`,
 }
