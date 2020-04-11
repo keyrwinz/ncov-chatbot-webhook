b71843f96da462e205409402372543fa1823b2c6
keyrwinz <keyrwinfelisilda@gmail.com>
2020-04-11 14:40:12 +0800

    added percentage value

--- Guide/covid-message.js
+++ Guide/covid-message.js
@@ -23,23 +23,28 @@ const generateMessage = (data, text = 'today') => {
     casesPerOneMillion
   } = data
 
+  const casesPercentage = cases / tests
+  const deathPercentage = deaths / tests
+  const recovPercentage = recovered / tests
+
   const flag = country ? countryInfo.iso2 ? emojiFlags.countryCode(countryInfo.iso2).emoji : '🌎' : '🌎' 
   const message = `
 As of ${moment(updated).tz(timezone).format('llll Z')}
 in ${country ? country === 'World' ? `the ${country}` : country : 'the World'} ${flag}
 Updated ${moment(updated).tz(timezone).fromNow()}
 
-Total cases ${text}: ${numeral(cases).format(0,0)}
+Total tests ${text}: ${numeral(tests).format(0,0)}
+
+Total cases ${text}: ${numeral(cases).format(0,0)} (${numeral(casesPercentage).format('0.00%')})
 with ${text}'s new cases: ${numeral(todayCases).format(0,0)}
 
-Total Deaths: ${numeral(deaths).format(0,0)}
+Total Deaths: ${numeral(deaths).format(0,0)} (${numeral(deathPercentage).format('0.00%')})
 with ${text}'s new deaths: ${numeral(todayDeaths).format(0,0)}
 
-Total Recovered: ${numeral(recovered).format(0,0)}
+Total Recovered: ${numeral(recovered).format(0,0)} (${numeral(recovPercentage).format('0.00%')})
 
 Active ${text}: ${numeral(active).format(0,0)}
 Critical: ${numeral(critical).format(0, 0)}
-Total tests ${text}: ${numeral(tests).format(0,0)}
 
 ${affectedCountries ? `Affected countries: ${affectedCountries}` : ''}
 `
